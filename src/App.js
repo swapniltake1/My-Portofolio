96a2dc3124c5f9de48e73ce201d87d853094b5b9
swapnil take <swapniltake1@outlook.com>
2024-04-11 14:57:42 +0530

    Integrated@vercel/speed-insights component into App.js for performance monitoring

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import About from './routes/About';
 import Contact from './routes/Contact';
 import Project from './routes/Project';
 import { Route, Routes } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
-
-
-
   return (
     <>
-    <Routes>
-    <Route path="/" element={<Home />}  />
-      <Route path="/swapniltake/" element={<Home />}  />
-      <Route path="/swapniltake/about" element={<About />}  />
-      <Route path="/swapniltake/contact" element={<Contact />}  />
-      <Route path="/swapniltake/project" element={<Project />}  />
-    </Routes>
+      <SpeedInsights />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/swapniltake/" element={<Home />} />
+        <Route path="/swapniltake/about" element={<About />} />
+        <Route path="/swapniltake/contact" element={<Contact />} />
+        <Route path="/swapniltake/project" element={<Project />} />
+      </Routes>
     </>
   );
 }
